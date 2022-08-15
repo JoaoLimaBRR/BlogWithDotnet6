d963d07d006c234d7407d0aee55b011a86ee3dbc
Joao Lima <joaovitorsoareslima12@gmail.com>
2022-08-14 23:13:38 -0300

    inclusao get by id em controller categoria

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -7,10 +7,20 @@ namespace Blog.Controllers
     [ApiController]
     public class CategoryController : ControllerBase
     {
-        [HttpGet("categories")]
+        [HttpGet("v1/categories")]
         public async Task<IActionResult> GetAsync([FromServices] BlogDataContext context)
         {
             return Ok(await context.Categories.ToListAsync());
+        }
+        [HttpGet("v1/categories/{id:int}")]
+        public async Task<IActionResult> GetAsync([FromServices] BlogDataContext context,[FromRoute] int id)
+        {
+            var category = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (category == null)  
+                return NotFound();
+
+            return Ok(category);
         }
     }
 }
