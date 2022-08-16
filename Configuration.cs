53a675feee31b0d7543e113f5a50ae9835e5b578
Joao Lima <joaovitorsoareslima12@gmail.com>
2022-08-16 00:23:17 -0300

    Inclusão de autenticacao por ApiKey

--- Configuration.cs
+++ Configuration.cs
@@ -3,6 +3,8 @@
     public class Configuration
     {
         //TOKEN - JWT - JSON WEB TOKEN
-        public static String JwtKey { get; set; } = "IzqjKiXvbU+S1cerdURAnQ==";
+        public static string JwtKey { get; set; } = "IzqjKiXvbU+S1cerdURAnQ==";
+        public static string ApiKeyName = "api_key";
+        public static string ApiKey = "api_keyV4jGUd0MBEOGnt6zbI/eKA==";
     }
 }
