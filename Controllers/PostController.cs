30025d04fe105a142292dd4b960a22ce5fdbfc90
Joao Lima <joaovitorsoareslima12@gmail.com>
2022-08-19 09:06:28 -0300

    Paginação de dados

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -1,4 +1,5 @@
 ﻿using Blog.Data;
+using Blog.ViewModels;
 using Blog.ViewModels.Posts;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -9,23 +10,51 @@ namespace Blog.Controllers
     public class PostController : ControllerBase
     {
         [HttpGet("v1/posts")]
-        public async Task<IActionResult> GetAllAsync([FromServices] BlogDataContext dbContext)
+        public async Task<IActionResult> GetAllAsync([FromServices] BlogDataContext dbContext, [FromQuery] int page = 0, [FromQuery] int pageSize = 25)
         {
-            return Ok(await dbContext
-                .Posts
-                .AsNoTracking()
-                .Include(x => x.Category)
-                .Include(x => x.Author)
-                .Select(x => new ListPostViewModel
-                {
-                    Id = x.Id,
-                    Title = x.Title,
-                    Slug = x.Slug,
-                    LastUpdateDate = x.LastUpdateDate,
-                    Category = x.Category.Name,
-                    Author = x.Author.Name
-                })
-                .ToListAsync());
+
+            try
+            {
+                var postList = await dbContext
+                                .Posts
+                                .AsNoTracking()
+                                .Include(x => x.Category)
+                                .Include(x => x.Author)
+                                .Select(x => new ListPostViewModel
+                                {
+                                    Id = x.Id,
+                                    Title = x.Title,
+                                    Slug = x.Slug,
+                                    LastUpdateDate = x.LastUpdateDate,
+                                    Category = x.Category.Name,
+                                    Author = x.Author.Name
+                                })
+                                //pula a quantidade de casas passadas por parametro
+                                .Skip(page * pageSize)
+                                //pega uma quantidade de registro passada por parametro
+                                .Take(pageSize)
+                                .ToListAsync();
+
+                var listSize = await dbContext.Posts.CountAsync();
+
+                return Ok(new ResultViewModel<dynamic>
+                    (new
+                    {
+                        listSize,
+                        page,
+                        pageSize,
+                        postList
+                    }
+                    ));
+
+
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(500, new ResultViewModel<string>("500P1 - Falha interna no servidor"));
+            }
+
         }
     }
 }
